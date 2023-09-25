aeda036d5deca21635aae9b746c37441e23f8c31
Anchal <Anchal@DESKTOP-33OJO2E>
2023-09-25 00:41:54 -0700

    #Passing Data from the Controller to the View#

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -15,8 +15,16 @@ public class HelloWorldController : Controller
     // GET: /HelloWorld/Welcome/ 
     // GET: /HelloWorld/Welcome?name=Rick&numtimes=4
     // Requires using System.Text.Encodings.Web;
-    public string Welcome(string name, int numTimes = 1)
+    //public string Welcome(string name, int numTimes = 1)
+    //{
+    //    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+    //}
+
+    // GET: HelloWorld/Welcome?name=Rick&numtimes=4
+    public IActionResult Welcome(string name, int numTimes = 1)
     {
-        return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        ViewData["Message"] = "Hello " + name;
+        ViewData["NumTimes"] = numTimes;
+        return View();
     }
 }
